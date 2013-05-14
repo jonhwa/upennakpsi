c302faf3f831846b9adf23e1a36bfc70a5afac2d
Jonathan Hwa <jonhwa@gmail.com>
2013-05-14 13:25:58 -0400

    Created app using Refinery CMS:

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Upennakpsi::Application.config.secret_token = '5c59092990385eb0d9d2ecde901c3371ba1c3efa0f249440a3a1ee88968c0a146d0439e5d2b77d6134f782bf3dd8c67a8bdcb8582482fd041d12fd592aff11ec'
